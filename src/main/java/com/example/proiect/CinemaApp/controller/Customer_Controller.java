f8e70664fc17e61869bc993e9febb8ae2812e6b3
bogdan <bogdan.calburean2004@gmail.com>
2025-10-28 16:56:14 +0200

    Controller

---
+++ src/main/java/com/example/proiect/CinemaApp/controller/Customer_Controller.java
@@ -0,0 +1,4 @@
+package com.example.proiect.CinemaApp.controller;
+
+public class Customer_Controller {
+}
---
+++ src/main/java/com/example/proiect/CinemaApp/controller/Movie_Controller.java
@@ -0,0 +1,4 @@
+package com.example.proiect.CinemaApp.controller;
+
+public class Movie_Controller {
+}
