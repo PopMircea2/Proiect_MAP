e0d5d6600db459dc5bbb8109ad6ee5186a77c4ca
bogdan <bogdan.calburean2004@gmail.com>
2025-11-03 13:14:03 +0200

    Templates Final

--- src/main/java/com/example/proiect/CinemaApp/controller/Controller.java
+++ src/main/java/com/example/proiect/CinemaApp/controller/Controller.java
@@ -4,12 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
-    @org.springframework.stereotype.Controller
-    public class Controller {
-        @GetMapping("/hello")
-        @ResponseBody
-        public String sayHello() {
-            return "Die Anwendung funktioniert!";
-        }
+@org.springframework.stereotype.Controller
+public class Controller {
+    @GetMapping("/")
+    public String home() {
+        return "index";
     }
 
+    @GetMapping("/hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Die Anwendung funktioniert!";
+    }
+}
