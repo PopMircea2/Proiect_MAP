890c7c48f1d2e9d5f56baa014203e9c1d9e2b9eb
bogdan <bogdan.calburean2004@gmail.com>
2025-11-04 19:40:12 +0200

    Fixes

--- src/main/java/com/example/proiect/CinemaApp/controller/SupportStaffController.java
+++ src/main/java/com/example/proiect/CinemaApp/controller/SupportStaffController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/supportstaffs")
+@RequestMapping("/supportstaff")
 public class SupportStaffController {
 
     private final SupportStaffService supportStaffService;
@@ -24,19 +24,19 @@ public class SupportStaffController {
 
     @GetMapping("/new")
     public String showCreateForm(Model model) {
-        model.addAttribute("staff", new SupportStaff());
+        model.addAttribute("supportStaff", new SupportStaff());
         return "supportstaff/form";
     }
 
     @PostMapping
     public String addSupportStaff(@ModelAttribute SupportStaff staff) {
         supportStaffService.addSupportStaff(staff);
-        return "redirect:/supportstaffs";
+        return "redirect:/supportstaff";
     }
 
     @PostMapping("/{id}/delete")
     public String deleteSupportStaff(@PathVariable String id) {
         supportStaffService.deleteSupportStaff(id);
-        return "redirect:/supportstaffs";
+        return "redirect:/supportstaff";
     }
 }
